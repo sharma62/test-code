f5b3ac7f52ee748e6fd9d2a88b06eddd881eb26c
SURAJ KUMAR SHARMA <75290099+sharma62@users.noreply.github.com>
2026-02-02 11:59:48 +0000

    intro VIP rule in App.jsx go & check

--- check Mern/src/App.jsx
+++ check Mern/src/App.jsx
@@ -7,16 +7,24 @@ import './App.css'
 import { CheckCustomHook } from './components/CheckCustomHook'
 
 function App() {
- 
 
-
+  console.log('2' + '2' * '2' / 2) //2
+  /*
+   🔑 Rule to Remember (VERY IMPORTANT)
+     -- Operator	Behavior --
+          +	String concatenation if any operand is string
+          -, *, /	Always convert to number
+          
+      🧠 One‑Line Explanation (Interview Ready)
+      The + operator concatenates strings, but the - operator forces numeric conversion, which results in 22 - 2 = 20.
+*/
   return (
     <>
       {/* <Reducer/> */}
       {/* <Memo/> */}
       {/* <Callback/> */}
       {/* <LayoutEffect/> */}
-      <CheckCustomHook/>
+      <CheckCustomHook />
 
     </>
   )
